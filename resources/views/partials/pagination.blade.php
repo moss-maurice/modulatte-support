605657ed8780417fd59a43c6bca8ae900b0ba72b
mmaurice <kreexus@yandex.ru>
2024-01-09 16:28:02 +0400

    pagination fixes

--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -5,7 +5,7 @@
 
 @if ($list->lastPage() > 1)
     <div class="pagination d-flex justify-content-center align-items-baseline">
-        @if ($catalog->currentPage() > 1)
+        @if ($list->currentPage() > 1)
             <a class="btn first" href="{{ $list->url(1) }}" aria-label="First">
                 <span aria-hidden="true">Первая</span>
             </a>
@@ -15,7 +15,7 @@
         @endif
 
         @for ($i = max(($list->currentPage() - $distance), 1); $i <= min(($list->currentPage() + $distance), $list->lastPage()); $i++)
-            @if ($pagination->get('current') == $i)
+            @if ($list->currentPage() == $i)
                 <span class="page-item num active bg-secondary">{{ $i }}</span>
             @else
                 <a class="page-item num" href="{{ $list->url($i) }}">{{ $i }}</a>
