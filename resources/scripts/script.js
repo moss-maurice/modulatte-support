9cd5d5f5fc7cd57e8e77faca7490eb10d83045f0
mmaurice <kreexus@yandex.ru>
2024-01-10 12:13:50 +0400

    scripts fixes

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -6,4 +6,28 @@ jQuery("document").ready(function () {
     jQuery("form").submit(function () {
         window.documentDirty = false;
     });
+
+    jQuery("i.star").on("click", function () {
+        let starWrapper = jQuery(".starWrapper");
+        let starEmpty = "fa-star-o";
+        let starFilled = "fa-star";
+        let stars = Number(
+            jQuery(this)
+                .attr("id")
+                .replace(/[^0-9]/g, "")
+        );
+
+        jQuery(starWrapper)
+            .find("i")
+            .each(function (index, item) {
+                jQuery(item).removeClass(starEmpty, starFilled);
+                if (index + 1 <= stars) {
+                    jQuery(item).addClass(starFilled);
+                } else {
+                    jQuery(item).addClass(starEmpty);
+                }
+            });
+
+        jQuery(starWrapper).find("input").val(stars);
+    });
 });
