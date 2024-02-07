c1c5fc4a24e104c6dbbe16f1c8db697f3e7213fd
mmaurice <kreexus@yandex.ru>
2024-02-07 15:05:25 +0400

    update fields

--- resources/views/partials/builder/form/fields/inputCheckbox.blade.php
+++ resources/views/partials/builder/form/fields/inputCheckbox.blade.php
@@ -1,9 +1,10 @@
 <!-- modulatte.partials.builder.form.fields.inputCheckbox -->
 @php
     $required = (isset($required) ? $required : false);
+    $label = (isset($label) ? $label : 'Да');
 @endphp
 
-<input id="enable" type="checkbox" name="enable" value="1" @if ($value) checked @endif />
-<label for="enable">Да</label>
+<input id="enable" type="checkbox" name="{{ $name }}" value="1" @if ($value) checked @endif />
+<label for="enable">{{ $label }}</label>
 <div class="form-text text-muted comment">{{ $comment }}</div>
 <!-- / modulatte.partials.builder.form.fields.inputCheckbox -->
