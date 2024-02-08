18250aca3efe00d511dfab52b4e79357642f213c
mmaurice <kreexus@yandex.ru>
2024-02-08 11:08:37 +0400

    fields updates

--- resources/views/partials/builder/table/fields/body/text.blade.php
+++ resources/views/partials/builder/table/fields/body/text.blade.php
@@ -3,6 +3,7 @@
     $colspan = (isset($colspan) ? $colspan : 1);
     $rowspan = (isset($rowspan) ? $rowspan : 1);
     $value = (!is_null($value) && ($value !== '') ? trim($value) : '—');
+    $class = (isset($class) ? $class : '');
 @endphp
 
 <td class="tableItem {{ $name }} {{ $class }}" data-field="{{ $name }}"{{ ($colspan > 1 ? " colspan=\"{$colspan}\"" : '') }}{{ ($rowspan > 1 ? " rowspan=\"{$rowspan}\"" : '') }}>
