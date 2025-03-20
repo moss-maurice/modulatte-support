7130ab8a5cf2d8de2e4aa3ed99b3eec93f004641
Moss Maurice <kreexus@gmail.com>
2025-03-20 12:23:58 +0400

    update price blade

--- resources/views/partials/builder/table/fields/body/price.blade.php
+++ resources/views/partials/builder/table/fields/body/price.blade.php
@@ -6,7 +6,8 @@
     $decimals  = (isset($decimals ) ? $decimals : 2);
     $decimalSeparator = (isset($decimalSeparator ) ? $decimalSeparator : '.');
     $thousandsSeparator  = (isset($thousandsSeparator ) ? $thousandsSeparator : ' ');
-    $value = (!is_null($value) && ($value !== '') ? trim("{$symbol} " . number_format($value, $decimals , $decimalSeparator, $thousandsSeparator)) : '—');
+    $symbolBefore = (isset($symbolBefore ) ? $symbolBefore : true);
+    $value = (!is_null($value) && ($value !== '') ? trim(($symbolBefore ? "{$symbol} " : "") . number_format($value, $decimals , $decimalSeparator, $thousandsSeparator) . (!$symbolBefore ? " {$symbol}" : "")) : '—');
 @endphp
 
 <td class="tableItem {{ $name }} {{ $class }}" data-field="{{ $name }}"{{ ($colspan > 1 ? " colspan=\"{$colspan}\"" : '') }}{{ ($rowspan > 1 ? " rowspan=\"{$rowspan}\"" : '') }}>
